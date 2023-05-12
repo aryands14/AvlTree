d71a03e12333b1b8ebeeeac5a835bd4673a46e80
Alvaro Bermudez Gamez <alvarobermudezgamez@uma.es>
2023-05-12 12:27:58 +0200

    refactor AvlNodeTest.java

--- src/test/java/avl/AvlNodeTest.java
+++ src/test/java/avl/AvlNodeTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * Created with IntelliJ IDEA. User: Antonio J. Nebro Date: 09/07/13 Time: 15:29
@@ -27,26 +30,26 @@ public class AvlNodeTest {
   @Test
   @DisplayName("setLeft method inserts the child node on the left of the parent node.")
   public void testHasLeft() {
-    assertFalse(node.hasLeft(), "testHasLeft");
+    assertThat(node.hasLeft()).isFalse();
     AvlNode<Integer> node2 = new AvlNode<Integer>(6);
     node.setLeft(node2);
-    assertTrue(node.hasLeft(), "testHasLeft");
+    assertThat(node.hasLeft()).isTrue();
   }
 
   @Test
   @DisplayName("setRight method inserts the child node on the right of the parent node.")
   public void testHasRight() {
-    assertFalse(node.hasRight(), "testHasRight");
+    assertThat(node.hasRight()).isFalse();
     AvlNode<Integer> node2 = new AvlNode<Integer>(6);
     node.setRight(node2);
-    assertTrue(node.hasRight(), "testHasRight");
+    assertThat(node.hasRight()).isTrue();
   }
 
   @Test
   @DisplayName("node only has left child")
   public void testOnlyHasRightChild(){
     node.setRight(new AvlNode<>(6));
-    assertTrue(node.hasOnlyARightChild());
+    assertThat(node.hasOnlyARightChild()).isTrue();
   }
 
   @Test
@@ -54,7 +57,7 @@ public class AvlNodeTest {
   public void shouldSetHeight() {
     int expectedHeight = 1000213;
     node.setHeight(expectedHeight);
-    assertEquals(expectedHeight, node.getHeight(), "Height is different from expected.");
+    assertThat( node.getHeight()).isEqualTo(expectedHeight);
   }
 
 }
